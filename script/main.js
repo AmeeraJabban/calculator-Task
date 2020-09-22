4deef6605ca6caf1981ba59ac9256276870329d1
ameera <ameera.a.jabban@gmail.com>
2020-09-22 16:43:36 +0300

    add all files

---
+++ script/main.js
@@ -0,0 +1,92 @@
+class CalculatorUI{
+  constructor(){
+    this.Engine;
+    this.DisplayMessageEl;
+  } 
+  init=()=>{ 
+    this.Engine=new CalculatorEngine();
+    this.DisplayMessageEl=document.querySelector(".result"); 
+    document.querySelector(".Equal").addEventListener("click",this.OnEqual_Click); 
+    document.querySelector(".reset").addEventListener("click",this.OnReset_Click);  
+    var NumbersEl= document.querySelectorAll(".Num");
+    for(var i=0; i<NumbersEl.length; i++){
+      NumbersEl[i].addEventListener("click",this.OnNumbers_Click);  
+    }       
+    var OperatorEl=document.querySelectorAll(".Oper");
+    for(var i=0; i<OperatorEl.length; i++){
+      OperatorEl[i].addEventListener("click",this.OnOperator_Click); 
+    }       
+  }
+  OnNumbers_Click=(e)=>{
+    this.DisplayMessage(this.Engine.SetNumbers(e.target.value));
+  }
+  OnOperator_Click=(e)=>{
+    this.DisplayMessage(this.Engine.SetOperator(e.target.value));
+  }
+  OnEqual_Click=()=>{
+    this.DisplayMessage(this.Engine.Calculate());
+  }
+  OnReset_Click=()=>{
+    this.Engine.Reset();
+    this.DisplayMessage('');
+  }
+  DisplayMessage=(Message)=>{
+    this.DisplayMessageEl.innerHTML=Message;
+  }
+}
+class CalculatorEngine{
+  constructor(){
+    this.FirstNum='';
+    this.SecondNum='';
+    this.Operator='';
+    this.PutInSeconedNum=false;
+  }
+  SetNumbers=(Value)=>{  
+    if(this.PutInSeconedNum){
+      this.SecondNum+=Value;
+      return this.SecondNum;
+    }
+    else{
+      this.FirstNum+=Value;  
+      return this.FirstNum;
+    }
+  }
+  SetOperator=(Value)=>{
+    this.Operator=Value;
+    this.PutInSeconedNum=true;
+    return this.Operator;
+  }
+  Calculate=()=>{
+    this.FirstNum=parseInt(this.FirstNum);
+    this.SecondNum=parseInt(this.SecondNum);
+    var Result=0;
+    switch (this.Operator){
+      case "+":
+        Result=this.FirstNum + this.SecondNum;
+        break;
+      case "-":
+        Result=this.FirstNum - this.SecondNum;
+        break;
+      case "*":
+        Result=this.FirstNum * this.SecondNum;
+        break;
+      case "/":
+        Result=this.FirstNum / this.SecondNum;
+        break;
+    } 
+    this.Reset();
+    return Result;
+  }  
+  Reset=()=>{
+    this.FirstNum="";
+    this.SecondNum="";
+    this.Operator="";
+    this.PutInSeconedNum=false;
+  }
+}
+var Calculator=new CalculatorUI();
+document.onreadystatechange =()=>{
+  if (document.readyState== "complete"){
+    Calculator.init();
+  }
+} 
